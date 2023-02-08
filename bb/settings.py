9490e519808860d51714c7b9815ed58a74a2ac6d
sycla <91137677+sycla@users.noreply.github.com>
2023-02-08 14:09:46 +0500

    Update settings.py

--- bb/settings.py
+++ bb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n$p!7pukq0=uo60&5xhrv9$wi*_r)kqrwl$)vigj-$^rd6voua
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ecom-production-e543.up.railway.app']
 
 
 # Application definition
